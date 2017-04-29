fd45416d904cfad84ab7336279a2aa09f5bc6387
planet139 <planet139@gmail.com>
2017-04-29 17:31:18 +0900

    add data, change code
    dd

--- typhoon-v2.py
+++ typhoon-v2.py
@@ -3,17 +3,19 @@ import numpy as np
 
 
 tf.set_random_seed(777)
-def xavier_init(n_inputs, n_outputs):
+def xavier_init(n_inputs, n_outputs, uniform = True):
     if uniform:
         init_range = tf.sqrt(6.0/ (n_inputs + n_outputs))
         return tf.random_uniform_initializer(-init_range, init_range)
 
 
 xy = np.loadtxt('train.csv', delimiter=',', dtype=np.float32)
-x_training_data = xy[:, 0:-1]
-y_training_data = xy[:, [-1]]
+x_training_data = xy[:-5, 0:-1]
+y_training_data = xy[:-5, [-1]]
+x_testing_data = xy[-5:, 0:-1]
+y_testing_data = xy[-5:, [-1]]
 
-x_arg = 3
+x_arg = 4
 location = 400
 
 X = tf.placeholder(tf.float32, [None, x_arg])
@@ -40,7 +42,7 @@ hypothesis = tf.nn.softmax(logits)
 # Cross entropy cost/loss
 cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y_one_hot)
 cost = tf.reduce_mean(cost_i)
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0005).minimize(cost)
 
 prediction = tf.argmax(hypothesis, 1)
 correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
@@ -48,16 +50,16 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Launch graph
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+        sess.run(tf.global_variables_initializer())
 
-    for step in range(20000):
-        sess.run(optimizer, feed_dict={X: x_training_data, Y: y_training_data})
-        if 0 == step % 5000:
-            loss, acc = sess.run([cost, accuracy], feed_dict={X: x_data, Y: y_data})
-            print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format(step, loss, acc))
+        for step in range(40001):
+            sess.run(optimizer, feed_dict={X: x_training_data, Y: y_training_data})
+            if 0 == step % 5000:
+                loss, acc = sess.run([cost, accuracy], feed_dict={X: x_training_data, Y: y_training_data})
+                print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format(step, loss, acc))
 
-    pred = sess.run(prediction, feed_dict={X:x_data})
+        pred = sess.run(prediction, feed_dict={X:x_testing_data})
 
-    # y_data: (N,1) = flatten => (N, ) matches pred.shape
-    for p, y in zip(pred, y_data.flatten()):
-        print("[{}] Prediction: {} True Y: {}".format(p == int(y), p, int(y)))
+        # y_data: (N,1) = flatten => (N, ) matches pred.shape
+        for p, y in zip(pred, y_testing_data.flatten()):
+            print("[{}] Prediction: {} True Y: {}".format(p == int(y), p, int(y)))
